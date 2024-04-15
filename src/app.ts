b1dc4b84a8d27454ffcbc639d02430f25030ac5d
edenrorman <eden@tonkean.com>
2024-04-15 22:42:07 +0300

    enable cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import serverless from "serverless-http";
 import routes from "./routes";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/", routes);
